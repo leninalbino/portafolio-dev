776a77d31bd6605435f1fde8dfd9fb90ae1a2b71
lenin albino <llchavez@stefanini.com>
2024-09-19 00:03:08 -0500

    cracion del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './module/about/about.component';
+import { ExperienceComponent } from './module/experience/experience.component';
+import { SkillsComponent } from './module/skills/skills.component';
+import { EducationComponent } from './module/education/education.component';
+import { ContactComponent } from './module/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'education', component: EducationComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '', redirectTo: '/about', pathMatch: 'full' }
+];
+
